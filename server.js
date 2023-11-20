1ac77dbe41103590fd855d5240cae4c123167d62
siposromeo <sipos.romeo.bence@students.jedlik.eu>
2023-11-20 14:06:13 +0100

    v2

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+// +1 PONT
 const express = require('express')
 require('dotenv').config()
+const trainings = require('./routes/trainings')
+
 const app = express()
+
+app.use('/api/trainings', trainings)
+
 app.listen(process.env.PORT, console.log(`Server running on port ${process.env.PORT}`))
