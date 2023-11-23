d6666231dcbab1c862ba0a122b84727ac4a5db95
siposromeo <sipos.romeo.bence@students.jedlik.eu>
2023-11-23 09:39:00 +0100

    v2.06

--- controllers/trainings.js
+++ controllers/trainings.js
@@ -18,6 +18,7 @@ exports.getTraining = (req, res, next) => {
 // @route  POST /api/trainings
 // @access Private
 exports.createTraining = (req, res, next) => {
+  console.log(req.body);
   res.status(200).json({ success: true, msg: "Create new training" });
 }; 
 
--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express')
 require('dotenv').config()
 const trainings = require('./routes/trainings')
 
+
 const app = express()
 
+app.use(express.json())
 app.use('/api/trainings', trainings)
 
 app.listen(process.env.PORT, console.log(`Server running on port ${process.env.PORT}`))
